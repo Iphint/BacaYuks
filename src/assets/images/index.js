fb76a83bfb7d511b7e50ad88baa847cd8c179c78
Rozzi21 <fakhruddinarrozi21@gmail.com>
2024-01-05 07:57:26 +0700

    add quran logo

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,5 @@
 import BrightQuran from '../images/BrightQuran.png';
 import QuranDetail from '../images/QuranDetail.png';
+import QuranLogo from '../images/QuranDetail.png'
 
-export { BrightQuran, QuranDetail };
+export { BrightQuran, QuranDetail ,QuranLogo};
